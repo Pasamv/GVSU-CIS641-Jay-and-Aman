93b6a929561f69441431ce75d2328792dd3ca267
amanrsp <prajapam@mail.gvsu.edu>
2024-12-04 04:05:12 -0500

    Synced with backend the search and post api;s

--- src/frontend/src/pages/RecipeSearch.jsx
+++ src/frontend/src/pages/RecipeSearch.jsx
@@ -1,11 +1,15 @@
 import { useLocation } from "react-router-dom";
+import {GetRecipes} from "../utils/api"
+import RecipesList from "../components/RecipiesList/RecipiesList";
 
 function RecipeSearch()
 {
     const location = useLocation();
     const {query} = location.state || {};
+    const data=GetRecipes(query);
     return <>
-    <h1>Recipe Search {query}</h1>
+        <h1>Recipe Search Results for {query}</h1>
+        <RecipesList data={data}/>
     </>
 }
 export default RecipeSearch;
