976af078129f7ff0666fbcc386de9cf210b2e07f
pasamv <pasamv@mail.gvsu.edu>
2024-12-04 04:04:20 -0500

    Synced frontend & backend for POST and Search recipies.

--- src/backend/app/__init__.py
+++ src/backend/app/__init__.py
@@ -1,6 +1,6 @@
 from flask import Flask
-# from flask_cors import CORS
+from flask_cors import CORS
 app=Flask(__name__,static_folder='../images/')
-# CORS(app,origins=['http://localhost:3000'])
+CORS(app,origins=['http://localhost:3000'])
 from app import routes
 
--- src/backend/app/routes.py
+++ src/backend/app/routes.py
@@ -1,5 +1,6 @@
 from flask import request, jsonify
 from app import app
+from app import database
 
 @app.route('/')
 def index():
@@ -8,16 +9,20 @@ def index():
 @app.route('/post_recipe', methods=["POST"])
 def post_recipe():
     # user_id = request.header.get("user_id")
-    title=request.form.get("title")
-    ingredients=request.form.get("ingredients")
-    instructions=request.form.get("instructions")
+    data = request.get_json()
+    title=data.get("title")
+    ingredients=data.get("ingredients")
+    instructions=data.get("instructions")
     #user_name
     print(title, ingredients, instructions)
-    # database.add({"collection":"recipe", "user_name":username})
+    username="Jayanth"
+    database.create("recipes", {"user_name":username,"title":title,"ingredients":ingredients,"instructions":instructions})
     return jsonify({'message':'Successfully added the item to lending space'}), 200
 
 @app.route('/search',methods=["GET"])
 def search():
     recipe_name=request.args.get("title")
     print(recipe_name)
-    return jsonify(recipe_name=recipe_name),200
+    print(database.get("recipes",""))
+    return jsonify(recipe_name=database.get("recipes","")),200
+
