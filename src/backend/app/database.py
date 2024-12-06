24c8e2e9d61a542a0b1a1a64c79d2c28402be487
pasamv <pasamv@mail.gvsu.edu>
2024-12-06 14:24:34 -0500

    Created API's for signin and signup

--- src/backend/app/database.py
+++ src/backend/app/database.py
@@ -21,25 +21,50 @@ recipes=[
     "user_id":"234"
 },
 ]
-user=[
 
+users=[
+{
+    "user_id":123465,
+    "first_name": "Aman",
+    "last_name":"Prajapati",
+    "email":"prajapa@mail.gvsu.edu",
+    "password":"zzzzz",
+    "profile_pic":"some1.png"
+},
+{
+    "user_id":34729,
+    "first_name":"Jayanth",
+    "last_name":"Pasam",
+    "email":"pasamv@mail.gvsu.edu",
+    "password":"aaaaa",
+    "profile_pic":"some.png"
+}
 ]
+
 reviews=[
 {
-    "user_id":"kasik",
-    "review":"Awsome!",
+    "user_id":"Jayanth",
+    "review":"It's not as tasty as I expected",
     "recipe_id":"emcjdf"
 },
 {
-    "user_id":"kasik",
-    "review":"Awsome!",
+    "user_id":"Aman",
+    "review":"Good!",
     "recipe_id":"emcjdf"
 }
 ]
 
-def get(collection,search_string):
+def get(collection,condition):
     if(collection=="recipes"):
-        return recipes
+        if(condition.get("title")):
+            if(condition["title"]=="top"):
+                return recipes
+            recipes_matched=[]
+            for recipe in recipes:
+                if(condition["title"] in recipe["title"]):
+                    recipes_matched.append(recipe)
+            return recipes_matched
+    return []
 
 def create(collection,data):
     if(collection=="recipes"):
@@ -49,6 +74,8 @@ def create(collection,data):
     if(collection=="reviews"):
         reviews.append(data)
         return
+    if(collection=="users"):
+        users.append(data)
 
 def find(collection,condition):
     if(collection=="recipes"):
@@ -56,5 +83,18 @@ def find(collection,condition):
             if(recipe["recipe_id"]==condition):
                 return recipe
     if(collection=="reviews"):
-        return reviews
-    return ""
+        reviews_of_recipe=[]
+        for review in reviews:
+            if(review["recipe_id"]==condition['recipe_id']):
+                reviews_of_recipe.append(review)
+        return reviews_of_recipe
+    if(collection=="users"):
+        if(condition.get("email")):
+            for user in users:
+                if(user["email"]==condition["email"] and user["password"]==condition["password"]):
+                    return user
+        if(condition.get("user_id")):
+            for user in users:
+                if(user["user_id"]==int(condition["user_id"])):
+                    return user
+    return {}
--- src/backend/app/routes.py
+++ src/backend/app/routes.py
@@ -23,10 +23,10 @@ def post_recipe():
 
 @app.route('/search',methods=["GET"])
 def search():
-    recipe_name=request.args.get("title")
-    print(recipe_name)
-    print(database.get("recipes",""))
-    return jsonify(recipe_name=database.get("recipes","")),200
+    recipe_title=request.args.get("title")
+    print(recipe_title)
+    res = database.get("recipes",{"title":recipe_title})
+    return jsonify(res),200
 
 @app.route('/recipe/<recipe_id>',methods=["GET"])
 def recipe(recipe_id):
@@ -35,10 +35,10 @@ def recipe(recipe_id):
         return recipe,200
     return jsonify({}),200
 
-@app.route("/reviews/<review_id>",methods=["GET"])
-def reviews(review_id):
-    print(review_id)
-    res=database.find("reviews","review_id")
+@app.route("/reviews/<recipe_id>",methods=["GET"])
+def reviews(recipe_id):
+    print(recipe_id)
+    res=database.find("reviews",{"recipe_id":recipe_id})
     return jsonify(res),200
 
 @app.route('/post_review', methods=["POST"])
@@ -48,9 +48,38 @@ def post_review():
     data = request.get_json()
     review=data.get("review")
     recipe_id=data.get("recipe_id")
+    first_name=data.get("first_name")
     #user_name
     print(recipe_id, review)
-    username="Jayanth"
-    database.create("reviews", {"user_id":username,"recipe_id":recipe_id,"review":review, "review_id":count})
+    database.create("reviews", {"first_name":first_name,"recipe_id":recipe_id,"review":review, "review_id":count})
     count+=1
     return jsonify({'message':'Successfully added the review to the recipe'}), 200
+
+@app.route("/user",methods=["GET"])
+def user():
+    authorization_header=request.headers.get('Authorization')
+    if(authorization_header):
+        token = authorization_header.split(' ')[1]
+    print("getting user info and we recived user_id",token)
+    res=database.find("users",{"user_id":token})
+    print("we are returning",res)
+    return jsonify(res)
+
+@app.route("/signin",methods=["POST"])
+def signin():
+    data = request.get_json()
+    print("singin data we recived",data)
+    res=database.find("users",data)
+    print("we are returning",res)
+    if(res):
+        return jsonify(res),200
+    return jsonify("invalid credentials"),404
+
+@app.route('/signup',methods=['POST'])
+def signup():
+    global count
+    data = request.get_json()
+    print("signup data we recived",data)
+    data["user_id"]=count
+    database.create("users",data)
+    return jsonify(data),200
